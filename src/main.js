09f68b9a5bc191d009732671851852dbdc3a37a4
jef <esteronjeferson16@gmail.com>
2026-09-24 23:58:56 +0800

    BL: challenge 3 - states done?

--- src/main.js
+++ src/main.js
@@ -5,26 +5,46 @@ app.innerHTML = `
     <input type="text" name="search-name" placeholder="Search a book...">
     <button type="submit">Search</button>
   </form>
+  <div id="books-display"></div>
 `
 const searchForm = document.getElementById("search-form");
+const booksDisplay = document.getElementById("books-display");
 
 searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const formData = new FormData(searchForm);
 
-  const response = await fetch(`https://openlibrary.org/search.json?q=${formData.get("search-name")}`)
-  const data = await response.json();
+  displayLoadingScreen();
 
-  const books = data.docs;
+  try {
+    const response = await fetch(`https://openlibrary.org/search.json?q=${formData.get("search-name")}`)
+    const data = await response.json();
+    const books = data.docs;
 
-  for (let i = 0; i < 10; i++) {
-    app.insertAdjacentHTML("beforeend",`
-      <div class="book-container">
-        <img src="https://covers.openlibrary.org/b/id/${books[i].cover_i}-L.jpg" class="cover">
-        <h1 class="title">${books[i].title || "N/A"}</h1>
-        <h3 class="author-name">${books[i].author_name || "N/A"}</h3>
-        <h3 class="year-published">${books[i].first_publish_year || "N/A"}</h3>
-      </div>`);
+    booksDisplay.innerHTML = "";
+    for (let i = 0; i < Math.min(books.length, 10); i++) { 
+      booksDisplay.innerHTML += `
+        <div class="book-container">
+          <img src=${`https://covers.openlibrary.org/b/id/${books[i].cover_i}-L.jpg` || "/assets/No_Image_Available.jpg"} class="cover">
+          <h1 class="title">${books[i].title || "N/A"}</h1>
+          <h3 class="author-name">${books[i].author_name || "N/A"}</h3>
+          <h3 class="year-published">${books[i].first_publish_year || "N/A"}</h3>
+        </div>`;
+    }
+    console.log(books);
+
+    if (books.length === 0) {
+      booksDisplay.innerHTML = `<h1 class="empty-state" role="status" aria-live="polite">No Books Found</h1>`;
+    }
+  } catch (error) {
+    console.error("Error searching books");
+    booksDisplay.innerHTML = `<h1 class="error-state" role="alert">Something Went Wrong</h1>`;
   }
-  console.log(books);
+  
+  
 });
+
+
+function displayLoadingScreen() {
+  booksDisplay.innerHTML = `<h1 class="loading-state" role="status" aria-live="polite">Loading...</h1>`;
+}
