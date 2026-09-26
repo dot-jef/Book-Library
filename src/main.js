26ee78c9eb092047f51a42330fe41e9093976df0
jef <esteronjeferson16@gmail.com>
2026-09-26 22:51:53 +0800

    BL: challenge 3 properly done

--- src/main.js
+++ src/main.js
@@ -18,6 +18,10 @@ searchForm.addEventListener("submit", async (e) => {
 
   try {
     const response = await fetch(`https://openlibrary.org/search.json?q=${formData.get("search-name")}`)
+
+    if (!response.ok) {
+      throw new Error("Unable to fetch properly");
+    }
     const data = await response.json();
     const books = data.docs;
 
@@ -25,19 +29,19 @@ searchForm.addEventListener("submit", async (e) => {
     for (let i = 0; i < Math.min(books.length, 10); i++) { 
       booksDisplay.innerHTML += `
         <div class="book-container">
-          <img src=${`https://covers.openlibrary.org/b/id/${books[i].cover_i}-L.jpg` || "/assets/No_Image_Available.jpg"} class="cover">
+          <img src=${books[i].cover_i ? `https://covers.openlibrary.org/b/id/${books[i].cover_i}-L.jpg` : "/assets/No_Image_Available.jpg"} class="cover">
           <h1 class="title">${books[i].title || "N/A"}</h1>
           <h3 class="author-name">${books[i].author_name || "N/A"}</h3>
           <h3 class="year-published">${books[i].first_publish_year || "N/A"}</h3>
         </div>`;
     }
-    console.log(books);
+    console.log(response);
 
     if (books.length === 0) {
       booksDisplay.innerHTML = `<h1 class="empty-state" role="status" aria-live="polite">No Books Found</h1>`;
     }
   } catch (error) {
-    console.error("Error searching books");
+    console.error("Error found: ", error.message);
     booksDisplay.innerHTML = `<h1 class="error-state" role="alert">Something Went Wrong</h1>`;
   }
   
